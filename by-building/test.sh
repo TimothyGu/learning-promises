1ed0412fd294c909febd0222eca0d540a911d28a
Timothy Gu <timothygu99@gmail.com>
2022-08-18 00:08:00 -0700

    by-building: add a simple test case

---
+++ by-building/test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -exo pipefail
+
+steps=(
+  'step-1'
+  'step-2'
+  'step-3'
+  'step-4'
+  'step-5'
+  'step-6'
+  'step-7.1'
+  'step-7.2'
+  'step-7.3'
+  'step-8'
+)
+
+if [[ $(basename $PWD) == by-building || ! -d by-building ]]
+then
+  echo 'You must be in learning-promises/ for this script to work' >&2
+  exit 1
+fi
+
+for step in "${steps[@]}"
+do
+  node "by-building/$step/promises.js"
+done
