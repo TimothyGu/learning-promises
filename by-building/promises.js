40e2f788189f7e2d68256ff047189db8c5675ed4
Timothy Gu <timothygu99@gmail.com>
2022-08-18 00:00:31 -0700

    Step 2: Data model

--- by-building/promises.js
+++ by-building/promises.js
@@ -1,9 +1,20 @@
 class Promise {
   constructor() {
-    throw new Error('unimplemented');
+    // Part 1: container for a value yet to be populated.
+    this._state = 'pending'; // or 'fulfilled' or 'rejected'
+    this._value = undefined;
+
+    // Part 2: functions that should be called when the promise becomes fulfilled or rejected.
+    this._onFulfilled = [];
+    this._onRejected = [];
   }
 
   then(onFulfilled, onRejected) {
-    throw new Error('unimplemented');
+    if (typeof onFulfilled === 'function') {
+      this._onFulfilled.push(onFulfilled);
+    }
+    if (typeof onRejected === 'function') {
+      this._onRejected.push(onRejected);
+    }
   }
 }
