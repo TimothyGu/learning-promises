747233014fed760a42323a7518493ec7502b17aa
Timothy Gu <timothygu99@gmail.com>
2022-09-05 14:54:31 -0700

    Step 3: First steps

--- async-await/async-await.js
+++ async-await/async-await.js
@@ -7,6 +7,37 @@
  */
 function asyncRun(gen) {
   return new Promise((resolve, reject) => {
-    // TODO
+    const yieldedResult = gen.next();
+    if (yieldedResult.done) {
+      resolve(yieldedResult.value);
+      return;
+    }
+
+    yieldedResult.value.then((resolvedValue) => {
+      const yieldedResult = gen.next(resolvedValue);
+      // TODO: handle more yielded values
+    });
   });
 }
+
+// Test code
+
+function* functionWithReturnValue() {
+  console.log(new Date(), 'starting functionWithReturnValue');
+  return 100;
+}
+
+asyncRun(functionWithReturnValue())
+  .then((value) => {
+    console.log(new Date(), 'functionWithReturnValue() returned', value);
+  });
+console.log(new Date(), 'after starting functionWithReturnValue()');
+
+function* printYieldedValue() {
+  console.log(new Date(), 'starting printYieldedValue');
+  const value = yield Promise.resolve(42);
+  console.log(new Date(), 'got yielded value', value);
+}
+
+asyncRun(printYieldedValue());
+console.log(new Date(), 'after starting printYieldedValue()');
